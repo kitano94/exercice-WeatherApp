0561ee32c18a1e182e6afd2387aed746dd58adbe
Mehdi Yembou <yembou.mehdi@hotmail.fr>
2015-12-08 15:40:11 +0100

    Initial Commit

---
+++ exercice-WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exercice-WeatherApp
+//
+//  Created by Mehdi Yembou on 08/12/2015.
+//  Copyright © 2015 Mehdi Yembou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
